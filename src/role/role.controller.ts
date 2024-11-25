13f5fd39c91f662e10e939d433376d5a7f53d430
totskaiasonia <totskaiasonia@gmail.com>
2024-11-25 19:12:40 +0200

    add work with roles

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { RoleService } from './role.service';
+import { CreateRoleDto } from './dto/create-role.dto';
 
-@Controller('role')
+@Controller('roles')
 export class RoleController {
     constructor(private readonly roleService: RoleService) {}
+
+    @Get()
+    findAll() {
+        return this.roleService.findAll();
+    }
+    @Post()
+    create(@Body() createRoleDto: CreateRoleDto) {
+        return this.roleService.create(createRoleDto);
+    }
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.roleService.remove(+id);
+    }
 }
--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { RoleEntity } from 'src/entity/role.entity';
 import { EUserRole } from 'src/enum/role.enum';
 import { Repository } from 'typeorm';
+import { CreateRoleDto } from './dto/create-role.dto';
 
 @Injectable()
 export class RoleService {
@@ -11,7 +12,19 @@ export class RoleService {
         private roleRepository: Repository<RoleEntity>,
     ) {}
 
+    public async findAll() {
+        return await this.roleRepository.find();
+    }
     public async findOne(roleName: EUserRole) {
         return await this.roleRepository.findOneByOrFail({ name: roleName });
+    }
+    public async create(createRoleDto: CreateRoleDto) {
+        const role = await this.roleRepository.create({
+            name: EUserRole.ADMIN
+        });
+        return this.roleRepository.save(role);
+    }
+    public async remove(id: number) {
+        await this.roleRepository.delete(id);
     }
 }
