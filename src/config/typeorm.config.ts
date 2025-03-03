5d9900ea07a6c8dd8c4c48f200254c09a8c57fc2
totskaiasonia <totskaiasonia@gmail.com>
2025-03-03 15:15:37 +0200

    return db conn

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -12,15 +12,15 @@ export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
     ): Promise<TypeOrmModuleOptions> => {
         return {
             type: 'postgres',
-            host: configService.get<string>('DB_HOST', 'localhost'),
-            port: configService.get<number>('DB_PORT', 5432),
-            username: configService.get<string>('DB_USERNAME', 'postgres'),
-            password: configService.get<string>('DB_PASSWORD', 'Tos_11235'),
-            database: configService.get<string>('DB_DATABASE_NAME', 'primaflora_test'),
-            // url: configService.get<string>('DATABASE_URL'),
-            // ssl: {
-            //     rejectUnauthorized: false,
-            // },
+            // host: configService.get<string>('DB_HOST', 'localhost'),
+            // port: configService.get<number>('DB_PORT', 5432),
+            // username: configService.get<string>('DB_USERNAME', 'postgres'),
+            // password: configService.get<string>('DB_PASSWORD', 'Tos_11235'),
+            // database: configService.get<string>('DB_DATABASE_NAME', 'primaflora_test'),
+            url: configService.get<string>('DATABASE_URL'),
+            ssl: {
+                rejectUnauthorized: false,
+            },
             entities: [__dirname + '/../**/*.entity{.ts,.js}'],
             synchronize: true,
             // logging: true,
